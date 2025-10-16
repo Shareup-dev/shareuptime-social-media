2151be619e7c336ce384250a8f77ad399585aff9
ShareUpTime <shareuptimex@gmail.com>
2025-10-16 07:35:43 +0000

    chore(mobile): fix ESLint config; add Prettier; CI mobile lint (non-blocking)

--- mobile-app/.eslintrc.js
+++ mobile-app/.eslintrc.js
@@ -1,21 +1,14 @@
-module.exports = {module.exports = {
-
-  root: true,  root: true,
-
-  extends: [  extends: '@react-native-community',
-
-    '@react-native',};
-
-    '@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaFeatures: { jsx: true },
     ecmaVersion: 2020,
     sourceType: 'module',
   },
@@ -25,27 +18,11 @@ module.exports = {module.exports = {
     node: true,
   },
   rules: {
-    // TypeScript
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    
-    // React
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    
-    // General
-    'no-console': 'warn',
-    'prefer-const': 'error',
-    'no-var': 'error',
-    'object-shorthand': 'error',
   },
   settings: {
-    react: {
-      version: 'detect',
-    },
+    react: { version: 'detect' },
   },
 };
